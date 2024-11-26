6dba0e9f8e7cd02d2531908bc424daaca54dca3e
JI Sangho <Administrator@Selim-Sangho>
2024-11-26 10:52:02 +0900

    에러 맞추기 commit

--- src/test/java/kr/dogfoot/hwp2hwpx/MultipleSections.java
+++ src/test/java/kr/dogfoot/hwp2hwpx/MultipleSections.java
@@ -10,24 +10,18 @@ import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
 
-public class 빈파일 {
+public class MultipleSections {
     @Test
     public void test() throws Exception {
-        String testPath = "test/빈파일";
+        String testPath = "test/MultipleSections";
 
         HWPFile fromFile = HWPReader.fromFile(testPath + FileName.InputFile);
         HWPXFile toFile = Hwp2Hwpx.toHWPX(fromFile);
         HWPXWriter.toFilepath(toFile, testPath + FileName.OutputFile);
 
         {
-            String resultXML = Util.loadXMLString(testPath + "/result/header.xml", StandardCharsets.UTF_8);
-            String toXML = Util.zipFileString(testPath + FileName.OutputFile, "Contents/header.xml", StandardCharsets.UTF_8);
-            Assert.assertEquals(resultXML, toXML);
-        }
-
-        {
-            String resultXML = Util.loadXMLString(testPath + "/result/section0.xml", StandardCharsets.UTF_8);
-            String toXML = Util.zipFileString(testPath + FileName.OutputFile, "Contents/section0.xml", StandardCharsets.UTF_8);
+            String resultXML = Util.loadXMLString(testPath + "/result/content.hpf", StandardCharsets.UTF_8);
+            String toXML = Util.zipFileString(testPath + FileName.OutputFile, "Contents/content.hpf", StandardCharsets.UTF_8);
             Assert.assertEquals(resultXML, toXML);
         }
     }
